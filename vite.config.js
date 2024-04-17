5705788f1fc5630b5481b4341f3a8eb6690fe279
Deepanshu40 <gargdeepanshu40@gmail.com>
2024-04-17 06:25:39 +0530

    vite config modify

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        // target: 'http://localhost:8080',
+        target: 'https://getjob-backend-qa7t.onrender.com',
         changeOrigin: true,
       },
     },
