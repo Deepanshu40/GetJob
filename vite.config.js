fbd6087d96802e01f3d142d3de342eeffc26cd6c
Deepanshu40 <144712703+Deepanshu40@users.noreply.github.com>
2024-04-18 06:35:51 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'https://getjob-backend-qa7t.onrender.com/api',
-        changeOrigin: true,
+        // changeOrigin: true,
         // rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
